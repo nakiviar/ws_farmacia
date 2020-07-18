2136f586ed1248a82265d819d7a2ebcc0f3790f6
nakiviar <nakidevi@hotmail.com>
2020-07-18 15:14:24 -0500

    sube imagen por ID

--- ws.ciber.tienditas/src/main/java/ws/ciber/tienditas/rs/ProductoResource.java
+++ ws.ciber.tienditas/src/main/java/ws/ciber/tienditas/rs/ProductoResource.java
@@ -75,16 +75,16 @@ public class ProductoResource {
     
 
     @POST
-    @Path("/imagenes")
+    @Path("/imagenes/Subir/{productoId}")
     @Consumes({"image/jpeg", "image/png"})
-    public Response uploadImage(InputStream in, @HeaderParam("Content-Type") String fileType, @HeaderParam("Content-Length") long fileSize) throws IOException {
+    public Response uploadImage(@PathParam("productoId") String id,InputStream in, @HeaderParam("Content-Type") String fileType, @HeaderParam("Content-Length") long fileSize) throws IOException {
         
 
         if (fileSize > 1024 * 1024 * MAX_SIZE_IN_MB) {
             throw new WebApplicationException(Response.status(Status.BAD_REQUEST).entity("La imagen es más grande que " + MAX_SIZE_IN_MB + "MB").build());
         }
         
-        String fileName = "" + System.currentTimeMillis();
+        String fileName = id;
 
         if (fileType.equals("image/jpeg")) {
             fileName += ".jpg";
